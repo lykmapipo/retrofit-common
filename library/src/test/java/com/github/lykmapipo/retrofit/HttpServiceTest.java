5c60fea0ea58827a279933f42f9a9a6fa3586f80
lykmapipo <lallyelias87@gmail.com>
2019-04-28 12:26:24 +0300

    test: extract test class

--- library/src/test/java/com/github/lykmapipo/retrofit/HttpServiceTest.java
+++ library/src/test/java/com/github/lykmapipo/retrofit/HttpServiceTest.java
@@ -1,7 +1,5 @@
 package com.github.lykmapipo.retrofit;
 
-import com.google.gson.annotations.Expose;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -58,34 +56,5 @@ public class HttpServiceTest {
     public interface Api {
         @GET("/users")
         Call<List<User>> list();
-    }
-
-    public static class User {
-        @Expose
-        String name;
-
-        public User(String name) {
-            this.name = name;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-
-            User user = (User) o;
-
-            return name != null ? name.equals(user.name) : user.name == null;
-        }
-
-        @Override
-        public int hashCode() {
-            return name != null ? name.hashCode() : 0;
-        }
-
-        @Override
-        public String toString() {
-            return name;
-        }
     }
 }
